5aa5e858ff480baaa7e95c95c6151ed7fffaf68b
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-03 03:36:45 -0300

    add get and update task

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -8,9 +8,18 @@ export const getAllTasks = () => {
     return taskApi.get('tasks')
 }
 
+export const getTask = (id) => {
+    return taskApi.get(`tasks/${id}`)
+}
+
 export const createTask = (task) => {
     task.done = false;
     return taskApi.post('tasks/', task)
+}
+
+export const updateTask = (id, task) => {
+    task.done = false;
+    return taskApi.put(`tasks/${id}/`, task)
 }
 
 export const deleteTask = (id) => {
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,17 +1,35 @@
-// import React from 'react'
+import {useEffect, useState} from 'react'
 import {useNavigate, useParams} from "react-router-dom";
 import {useForm} from "react-hook-form";
-import { createTask , deleteTask} from "../api/tasks.api";
+import { createTask , deleteTask, getTask, updateTask} from "../api/tasks.api";
 
 const TaskFormPage = () => {
-  const {register, handleSubmit, formState: { errors}} = useForm();
+  const {register, handleSubmit, formState: { errors}, setValue} = useForm();
   const navigate = useNavigate()
   const params = useParams()
+  const [task, setTask] = useState({})
+
+  useEffect(()=>{
+    async function loadData(){
+      let res = await getTask(params.id);
+      setTask(res.data)
+      setValue('title', res.data.title)
+      setValue('description', res.data.description)
+    }
+    if(params.id)
+      loadData();
+
+  },[])
 
   const onSubmit = handleSubmit(async data =>{
     console.log(data);
-    const res = await createTask(data); // call to the api
-    console.log(res)
+    if(params.id){
+      const res = await updateTask(params.id, data); // call to the api
+    }else{
+      const res = await createTask(data); // call to the api
+    }
+    // console.log(res)
+    navigate('/tasks')
   })
 
   const onDelete = async (id)=>{
@@ -24,6 +42,7 @@ const TaskFormPage = () => {
 
   return (
     <div>
+      {task && <h1>{task.title}</h1>}
       <form onSubmit={onSubmit}>
         <input type="text" placeholder="title" {...register("title", {required: true})}/>
         {errors.title && <span>Ocurrio un error</span>}
