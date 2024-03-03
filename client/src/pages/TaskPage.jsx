68189f27fe0caeb6fd696faf9ebd2369d56365c2
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-03 01:31:39 -0300

    add TaskList component

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,8 +1,12 @@
 // import React from 'react'
+import TaskList from "../components/TaskList";
 
 const TaskPage = () => {
   return (
-    <div>TaskPage</div>
+    <>
+      <h2>Task Page </h2>
+      <TaskList />
+    </>
   )
 }
 
