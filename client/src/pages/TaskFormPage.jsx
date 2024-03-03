7d0aca7cbd65872c267a7f550eab751ca7725789
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-03 02:43:11 -0300

    add form with error handler

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,8 +1,28 @@
 // import React from 'react'
+import {useForm} from "react-hook-form";
 
 const TaskFormPage = () => {
+  const {register, handleSubmit, formState: { errors}} = useForm();
+
+  const onSubmit = handleSubmit( data =>{
+    console.log(errors);
+    console.log(data);
+    // return false;
+  })
+
   return (
-    <div>TaskFormPage</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="title" {...register("title", {required: true})}/>
+        {errors.title && <span>Ocurrio un error</span>}
+
+        <textarea rows="3" placeholder="description" {...register("description", {required: true})} ></textarea>
+        {errors.description && <span>Ocurrio un error</span>}
+        
+        <button>Save</button>
+      </form>
+
+    </div>
   )
 }
 
